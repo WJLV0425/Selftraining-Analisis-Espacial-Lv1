6e6bf79f015ab576d70e6ed240bd052f5a646270
Welifer Jr Lebron Vicente <53196530+WJLV0425@users.noreply.github.com>
2021-01-26 02:59:53 -0400

    Whats_going_on?_say_something

---
+++ Aed_Qgis_R_1.R
@@ -0,0 +1,14 @@
+### Book
+### APPLIED SPATIAL DATA ANALYSIS WITH R (BIVAND ET AL., 2013).
+
+## Autor: Welifer Lebrón
+## Fecha inicio: 21/01/21
+## Fecha conclusión:
+
+#Packages
+
+library(ctv)
+
+# BY INSTALLING  the "ctv" package and executing the command `install.views("Spatial")`, you will have almost all the packages needed to reproduce and visualise the content in this book.
+
+install.views("Spatial")
